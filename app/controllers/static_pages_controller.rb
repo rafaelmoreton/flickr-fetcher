c24a8c987e94aa052752e97ddcb03faf833ace1e
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-10-22 12:55:23 -0300

    Rescue app when an invalid user id is input

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
     flickr = Flickr.new
-    
-    @photos = flickr.people.getPublicPhotos({ user_id: params[:user_id] }) if params[:commit]
+
+      @photos = flickr.people.getPublicPhotos({ user_id: params[:user_id] }) if params[:commit]
+    rescue
+      flash[:alert] = "Couldn't find the user"
+      @user_id = params[:user_id]
+      redirect_to root_url(user_id: params[:user_id])
   end
 end
