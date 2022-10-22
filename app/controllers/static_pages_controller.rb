8fb7528169bcc8f4a9b3b07543625e60fd80117a
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-10-22 08:03:20 -0300

    Fetch photos from flickr and display them
    Use the user id submitted as a param on the request to fetch an XML with all
    photos of that user in the controller. The view them constructs an URL for each
    photo in the response.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def fetch_photos
+    flickr = Flickr.new
+    @photos = flickr.people.getPublicPhotos({ user_id: params[:user_id] })
+  end
 end
